bd96581bc9c450de1ea799db58b234a66a481c92
Michael Stack <n00b3rpwn4g3@gmail.com>
2025-07-04 05:51:46 -0400

    added some scenes, scripts, and assets

---
+++ Scripts/new_script.gd
@@ -0,0 +1,12 @@
+extends Node
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	print("it's morbin' time!")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	# process input
+	pass
